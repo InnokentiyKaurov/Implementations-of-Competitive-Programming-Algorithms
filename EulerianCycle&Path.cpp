a3c90923d7ea2c8feef43ad6e9b363bfa1a83790
InnokentiyKaurov <86614684+InnokentiyKaurov@users.noreply.github.com>
2021-09-11 11:50:53 +0300

    Update EulerianCycle&Path.cpp

--- EulerianCycle&Path.cpp
+++ EulerianCycle&Path.cpp
@@ -35,3 +35,49 @@ int main(){
   cout << endl;
 }
 
+// Eulerian Cycle (Undirected Graphs).
+// 1. Graph's edges must be in the same connected component (checked in the algo).
+// 2. deg[v] is even for any vertex v (not checked in the algo).
+// The algorithm also works for directed graphs.
+const int N = 2e5 + 10;
+
+vector<pair<int, int>> g[N];
+vector<int> ans;
+bool used[N];
+
+void dfs(int v){
+  for (auto [u, id] : g[v]){
+    if (used[id]) continue;
+    used[id] = 1;
+    dfs(u);
+    ans.pb(v);
+  }
+}
+
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  // cout.precision(20);
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < m; i++){
+    int a, b;
+    cin >> a >> b;
+    g[a].pb({b, i});
+    g[b].pb({a, i});
+  }
+  ans.pb(1);
+  dfs(1);
+  if (sz(ans) != m){
+    // The graph is not connected.
+  }
+  for (auto i : ans) cout << i << " ";
+  cout << endl;
+}
+
+// Eulerian Path
+// The algo above can be used. Run DFS from one of the terminal vertices (that have odd degree).
+// 1. Graph's edges must be in the same connected component (checked in the algo).
+// 2. Undirected: Exactly 0 or 2 vertices have odd degree, all other have even degree.
+//    Directed: Either all have indeg[x] = outdeg[x],
+//              or (one has indeg[v] - outdeg[v] = 1, and one has outdeg[u] - indeg[u] = 1, and all other have indeg[w] = outdeg[w]).
