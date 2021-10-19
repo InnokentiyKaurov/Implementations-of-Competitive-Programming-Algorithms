09d666aed2e98af1336eedf9dab61923df00a8e7
InnokentiyKaurov <86614684+InnokentiyKaurov@users.noreply.github.com>
2021-10-19 19:54:09 +0300

    Create DigitalTrie.cpp

---
+++ DigitalTrie.cpp
@@ -0,0 +1,27 @@
+const int S = 2, LOG = 30;
+
+bool bit(int x, int i){
+  return (x >> i) & 1;
+}
+
+struct Node{
+  vector<int> nxt;
+  Node() {
+    nxt.assign(S, -1);
+    cnt = 0;
+  }
+};
+
+vector<Node> trie(1);
+
+void add_num(int x){
+  int v = 0;
+  for (int i = LOG - 1; i >= 0; i--){
+    int cur = bit(x, i);
+    if (trie[v].nxt[cur] == -1){
+      trie[v].nxt[cur] = sz(trie);
+      trie.emplace_back();
+    }
+    v = trie[v].nxt[cur];
+  }
+}
