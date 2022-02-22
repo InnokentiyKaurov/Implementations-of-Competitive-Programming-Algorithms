5599f3a42d34121b8e0c3e81f97aaf5a200d15a8
InnokentiyKaurov <86614684+InnokentiyKaurov@users.noreply.github.com>
2022-02-22 18:00:42 +0100

    Update ConvexHullTrick.cpp

--- ConvexHullTrick.cpp
+++ ConvexHullTrick.cpp
@@ -1,3 +1,5 @@
+// Convex Hull Trick. Allows to insert a linear function to the hull in Ө(1) and get the minimum/maximum value of the stored function at a point in O(log n).
+// NOTE: The lines must be added in the order of decreasing/increasing gradients. CAREFULLY CHECK THE SETUP BEFORE USING!
 struct line{
   ll k, b;
   ll f(ll x){
@@ -14,7 +16,7 @@ void add_line(line nl){
   }
   while (sz(hull) > 1){
     auto& l1 = hull.end()[-2], l2 = hull.back();
-    if ((nl.b - l1.b) * (l2.k - nl.k) >= (nl.b - l2.b) * (l1.k - nl.k)) hull.pop_back(); // Default: decreasing k. For increasing k change the sign to <=.
+    if ((nl.b - l1.b) * (l2.k - nl.k) >= (nl.b - l2.b) * (l1.k - nl.k)) hull.pop_back(); // Default: decreasing gradient k. For increasing k change the sign to <=.
     else break;
   }
   hull.pb(nl);
