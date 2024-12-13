1b238d2f4ae12858790e192e93aa031c0635945b
ikaurov <iakaurov@gmail.com>
2024-12-13 15:09:58 -0500

    Added precomputation for Totient function

--- EulersTotient.cpp
+++ EulersTotient.cpp
@@ -11,3 +11,24 @@ ll phi(ll n){
   if (n > 1) ans -= ans / n;
   return ans;
 }
+
+
+// Precalculates totient function from 1 to N-1.
+// Complexity: O(N log log N).
+
+const int N = 2e5 + 20;
+
+int phi[N];
+
+void precalc(){
+  for (int i = 0; i < N; i++){
+    phi[i] = i;
+  }
+  for (int i = 2; i < N; i++){
+    if (phi[i] == i){
+      for (int j = i; j < N; j += i){
+        phi[j] -= phi[j] / i;
+      }
+    }
+  }
+}
